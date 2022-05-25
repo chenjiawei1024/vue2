361319bde5eb2a4d4f426fa4b08dc603dda75ee9
陈佳伟 <981872358@qq.com>
2022-05-25 17:31:54 +0800

    keep-alive组件完成注释

--- src/core/components/keep-alive.ts
+++ src/core/components/keep-alive.ts
@@ -12,10 +12,12 @@ type CacheEntry = {
 
 type CacheEntryMap = Record<string, CacheEntry | null>
 
+/* 优先获取组件的name字段，如果name不存在则获取组件的tag */
 function getComponentName(opts?: VNodeComponentOptions): string | null {
   return opts && (opts.Ctor.options.name || opts.tag)
 }
 
+// 解析include 和 exclude传入的值
 function matches(
   pattern: string | RegExp | Array<string>,
   name: string
@@ -54,6 +56,7 @@ function pruneCacheEntry(
   current?: VNode
 ) {
   const entry = cache[key]
+  /* 判断当前没有处于被渲染状态的组件，将其销毁*/
   if (entry && (!current || entry.tag !== current.tag)) {
     // @ts-expect-error can be undefined
     entry.componentInstance.$destroy()
@@ -67,6 +70,7 @@ const patternTypes: Array<Function> = [String, RegExp, Array]
 // TODO defineComponent
 export default {
   name: 'keep-alive',
+  // 抽象组件
   abstract: true,
 
   props: {
@@ -96,7 +100,9 @@ export default {
   },
 
   created() {
+    // this.cache是一个对象，用来存储需要缓存的组件 this.cache = {'key1':'组件1'}
     this.cache = Object.create(null)
+    //this.cache对象中的键值。
     this.keys = []
   },
 
@@ -106,6 +112,7 @@ export default {
     }
   },
 
+  // 在mounted钩子函数中观测 include 和 exclude 的变化
   mounted() {
     this.cacheVNode()
     this.$watch('include', val => {
@@ -120,14 +127,18 @@ export default {
     this.cacheVNode()
   },
 
+
   render() {
     const slot = this.$slots.default
+    // 在render函数中首先获取第一个子组件节点的 vnode
     const vnode = getFirstComponentChild(slot)
     const componentOptions = vnode && vnode.componentOptions
     if (componentOptions) {
       // check pattern
+      /* 首先获取该组件节点的名称 */
       const name = getComponentName(componentOptions)
       const { include, exclude } = this
+      /* 如果name与include规则不匹配或者与exclude规则匹配则表示不缓存，直接返回vnode */
       if (
         // not included
         (include && (!name || !matches(include, name))) ||
@@ -138,6 +149,7 @@ export default {
       }
 
       const { cache, keys } = this
+      // 获取组件的key
       const key =
         vnode.key == null
           ? // same constructor may get registered as different local components
@@ -145,17 +157,22 @@ export default {
             componentOptions.Ctor.cid +
             (componentOptions.tag ? `::${componentOptions.tag}` : '')
           : vnode.key
+      /* 如果命中缓存，则直接从缓存中拿 vnode 的组件实例 */
       if (cache[key]) {
         vnode.componentInstance = cache[key].componentInstance
         // make current key freshest
+        // 细节，为了当缓存数到达max后，能够删除最老的key
+        /* 调整该组件key的顺序，将其从原来的地方删掉并重新放在最后一个 */
         remove(keys, key)
         keys.push(key)
+        /* 如果没有命中缓存，则将其设置进缓存 */
       } else {
         // delay setting the cache until update
         this.vnodeToCache = vnode
         this.keyToCache = key
       }
 
+      /* 最后设置keepAlive标记位 */
       // @ts-expect-error can vnode.data can be undefined
       vnode.data.keepAlive = true
     }
